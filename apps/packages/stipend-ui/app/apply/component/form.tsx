7eb6c5a0fe778652dc541d019dc2e25f6f23022d
Maxwell Bloch <maxwellb@users.noreply.github.com>
2026-04-21 17:42:34 -0400

    Create and link the /apply page

---
+++ apps/packages/stipend-ui/app/apply/component/form.tsx
@@ -0,0 +1,10 @@
+
+export default async function ApplyForm() {
+    return (
+        <div>
+            <p className="font-extrabold text-xl m-20">
+                THE FORM GOES HERE
+            </p>
+        </div>
+    );
+}
---
+++ apps/packages/stipend-ui/app/component/cta.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Link from "next/link";
+
+export default function CTA() {
+    return (
+        <div className="mt-4">
+            <Link href="/apply"
+                className="bg-blue-400 text-white px-6 py-3 rounded-md font-bold">
+                Apply Now!
+            </Link>
+        </div>
+    );
+}
