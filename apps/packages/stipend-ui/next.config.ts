63c5fd05fd7d2771708ad2e454a1f1a92fa12d71
Maxwell Bloch <maxwellb@users.noreply.github.com>
2026-04-21 17:14:52 -0400

    Add MDX support

--- apps/packages/stipend-ui/next.config.ts
+++ apps/packages/stipend-ui/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+
+});
+
+const appConfig = withMDX(nextConfig);
+
+export default appConfig;
